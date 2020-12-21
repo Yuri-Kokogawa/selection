be795cec5d4a6c4ccbebd99fee635e0cd1a70ec5
Kokogawa Yuri <peaceproject19951226@gmail.com>
2020-12-21 17:03:57 +0000

    メッセージ

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -90,12 +90,11 @@
                     　　</div>
 
 
-                        <div class="form-group row mb-0　justify-content-center">
-                            <div class="col-md-6 offset-md-4　">
-                                <button type="submit" class="btn btn-primary ">
-                                    {{ __('messages.Register2') }}
-                                </button>
-                            </div>
+                        <div class="form-group row justify-content-center">
+                            <button type="submit" class="btn btn-primary ">
+                                {{ __('messages.Register2') }}
+                            </button>
+                        
                         </div>
                     </form>
                 </div>
@@ -104,3 +103,4 @@
     </div>
 </div>
 @endsection
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,15 +58,11 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ action('Admin\SelectController@add') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ action('Admin\SelectController@add') }}">
                                         {{ __('messages.My Page') }}
                                     </a>
                                     
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                    <a class="dropdown-item" href="{{ action('Admin\SelectController@add') }}">
                                         {{ __('messages.New Select') }}
                                     </a>
                                     
